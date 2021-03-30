fe8454e5f35b0f9d6c87fcde2c1580e58d974e93
Pahud <pahudnet@gmail.com>
2021-03-31 00:18:40 +0800

    add test

---
+++ src/__tests__/integ.simgple-lambda-edge.test.ts
@@ -0,0 +1,49 @@
+import * as path from 'path';
+import { SynthUtils } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
+import * as cf from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as cdk from '@aws-cdk/core';
+import * as extensions from '../extensions';
+
+
+test('minimal usage', () => {
+  // GIVEN
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'demo-stack');
+
+  // WHEN
+  // create the cloudfront distribution with extension(s)
+  const simple = new extensions.SimpleLambdaEdge(stack, 'SimpleLambdaEdge');
+
+  // create the cloudfront distribution with extension(s)
+  const dist = new cf.Distribution(stack, 'dist', {
+    defaultBehavior: {
+      origin: new origins.HttpOrigin('aws.amazon.com'),
+      edgeLambdas: [simple],
+    },
+  });
+
+  new cdk.CfnOutput(stack, 'distributionDomainName', {
+    value: dist.distributionDomainName,
+  });
+
+  // THEN
+  expect(SynthUtils.synthesize(stack).template).toMatchSnapshot();
+
+  expect(stack).toHaveResourceLike('AWS::CloudFront::Distribution', {
+    DistributionConfig: {
+      DefaultCacheBehavior: {
+        LambdaFunctionAssociations: [
+          {
+            EventType: 'viewer-request',
+            LambdaFunctionARN: {
+              Ref: 'SimpleLambdaEdgeFuncCurrentVersionC9DD846A24b685166479fd2dde8b72650070e396',
+            },
+          },
+        ],
+        ViewerProtocolPolicy: 'allow-all',
+      },
+    },
+  });
+});
